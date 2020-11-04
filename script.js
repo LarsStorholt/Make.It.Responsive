64c22414c5fff343b304a0312e193a1a79ea2c85
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2020-11-04 15:22:49 +0100

    about us nesten ferdig
    about us nesten ferdig

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-  window.addEventListener('load', (x) => {  var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-     this.classList.toggle("isActive");
-
+    this.classList.toggle("focus");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
   });
 }
-  });
